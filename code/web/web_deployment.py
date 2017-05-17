83be0e426e673335dae596b1cfbc895daa3d4e9d
Luis Garcia <loiis.x14@gmail.com>
2017-05-17 11:00:06 -0500

    Values variables addition.

--- code/web/web_deployment.py
+++ code/web/web_deployment.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def main_page():
+    getCrypto = request.form['exchange'] == 'Get ETH'
+    firstValue = request.form['firstCrypto']
+    secondValue = request.form['secondCrypto']
+    
     return render_template('mainView.html')
 
 if __name__ == "__main__":
