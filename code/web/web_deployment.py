0e0c38bdf618fba29db4bd010e05986a97a9bc02
Luis Garcia <luisgiii@Luiss-MacBook-Air.local>
2017-05-17 21:41:35 -0500

    Experimentation with Flask and Jinja.

--- code/web/web_deployment.py
+++ code/web/web_deployment.py
@@ -8,17 +8,17 @@ convertedAmount = ""
 @app.route("/")
 def main_page():
     global cryptoAmount, convertedAmount
-
     cryptoValue = request.args.get('firstCrypto')
     buyButton = request.args.get('exchange')
     if cryptoValue != None:
         cryptoAmount = cryptoValue
         print "Stored cdc: " + cryptoAmount
         #API call to convert to the other crypto.
+        convertedAmount = "0"
 
     if buyButton == "Get":
         print "Buy: " + cryptoAmount
-    return render_template('mainView.html', value1 = cryptoAmount)
+    return render_template('mainView.html', value1 = cryptoAmount, value2 = convertedAmount)
 
 if __name__ == "__main__":
     app.run()
