1f1c30aac974d3707f2b68f3b7b67d92df655e7a
Luis Garcia <loiis.x14@gmail.com>
2017-05-25 11:23:26 -0500

    Exchange conversion and comission added.

--- code/core/poloniexPrototype.py
+++ code/core/poloniexPrototype.py
@@ -35,8 +35,26 @@ def PoloniexTradingAPI(command, req={}):
     except urllib2.HTTPError as err:
         print err
 
+def calculateExchangeApproximate(amount, crypto):        
+    requiredCriptoValue = PoloniexTickerQuery("BTC_"+crypto) #! This will be in a separate thread or file.
+    if requiredCriptoValue != None:
+        return maxAmount = round(amount / float(requiredCriptoValue), 8)
+    
 def main():
-    print "main"
+    # User sets amount of BTC he wants to exchange.
+    print "Enter the amount of BTC you want to exchange:"
+    btcAmount = raw_input()
+    # User sets which crypto currency wants in exchange.
+    print "What crypto asset would you like to get?:"
+    exchangeCrypto = raw_input()
 
+    # We convert the 99% BTC to the requested crypto and separate the comission from here.
+    comission = float(float(btcAmount)*0.01)
+    convertedAmount = calculateExchangeApproximate(float(btcAmount)*0.99, exchangeCrypto) 
+    print "You will get approximately: " + convertedAmount + exchangeCrypto
+    
+    # Perform a deposit to Teilen account.
+    
+    
 if __name__ == "__main__":
     main()
