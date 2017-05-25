2099d48263a142dd7463c16173a69aa977a02a27
Luis Garcia <loiis.x14@gmail.com>
2017-05-25 10:55:15 -0500

    Core functionality in a cmd line interface.

---
+++ code/core/poloniexPrototype.py
@@ -0,0 +1,42 @@
+import urllib
+import urllib2
+import json
+import time
+import hmac,hashlib
+import requests
+
+# API credentials
+poloniex_key = ""
+poloniex_secret = ""
+
+availableCriptoArray = ["BTC_ETH","BTC_LSK"]
+
+def PoloniexTickerQuery(criptoAsset):
+    response = requests.get("https://poloniex.com/public?command=returnTicker")
+    if response.status_code != 403 and response.status_code != 404:
+        print (response)
+        json_data = json.loads(response.text)
+        return json_data[criptoAsset]["lowestAsk"]
+    else:
+        print response
+
+def PoloniexTradingAPI(command, req={}):
+    req['command'] = command
+    req['nonce'] = int(time.time()*1000)
+    encoded_data = urllib.urlencode(req)
+
+    sign = hmac.new(poloniex_secret, encoded_data, hashlib.sha512).hexdigest()
+    headers = {'Sign': sign, 'Key': poloniex_key}
+
+    try:
+        ret = urllib2.urlopen(urllib2.Request('https://poloniex.com/tradingApi', encoded_data, headers))
+        jsonRet = json.loads(ret.read())
+        return jsonRet
+    except urllib2.HTTPError as err:
+        print err
+
+def main():
+    print "main"
+
+if __name__ == "__main__":
+    main()
