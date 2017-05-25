d9e9ca5fca0b2e880736bdea19083a2f0fdc1697
Luis Garcia <luisgiii@Luiss-MacBook-Air.local>
2017-05-24 20:50:07 -0500

    AWS *.pem added, only use with Luis' EC2 Instance.

--- code/extras/launchAWSInstance.sh
+++ code/extras/launchAWSInstance.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i "/Users/luisgiii/Documents/Extras/AWS/testMachine.pem" ec2-user@ec2-52-11-116-130.us-west-2.compute.amazonaws.com
+ssh -vvv -i "testMachine.pem" ec2-user@ec2-52-11-116-130.us-west-2.compute.amazonaws.com
