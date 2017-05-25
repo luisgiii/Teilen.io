ce9475f32994c76a75a39cd63160f7a16ae929e5
Luis Garcia <luisgiii@Luiss-MacBook-Air.local>
2017-05-24 20:28:46 -0500

    Permissions changed plus the .pem path in my machine.

--- code/extras/launchAWSInstance.sh
+++ code/extras/launchAWSInstance.sh
@@ -1,2 +1,2 @@
-!# Only works in Luis' machine.
-ssh -i "testMachine.pem" ec2-user@ec2-52-11-116-130.us-west-2.compute.amazonaws.com
+#!/bin/bash
+ssh -i "/Users/luisgiii/Documents/Extras/AWS/testMachine.pem" ec2-user@ec2-52-11-116-130.us-west-2.compute.amazonaws.com
