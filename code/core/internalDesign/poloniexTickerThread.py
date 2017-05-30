e7ea7257b0e319c129386ab08ec47a05e33056cd
luisgiii <loiis.x14@gmail.com>
2017-05-30 18:47:31 -0500

    Forgot to upload changes.

---
+++ code/core/internalDesign/poloniexTickerThread.py
@@ -0,0 +1,22 @@
+import json
+import time
+import requests
+import os
+
+def PoloniexTickerCall(JSONfile):
+    response = requests.get("https://poloniex.com/public?command=returnTicker")
+    if response.status_code != 403 and response.status_code != 404:
+        json_data = json.loads(response.text)
+        JSONfile.dump(json_data)
+    else:
+        continue
+
+
+def main():
+    tickerData = open("tickerJSON", "wb")
+    for repeat in range(0,2):
+        PoloniexTickerCall(tickerData)
+        time.sleep(3)
+
+if __name__ == '__main__':
+    main()
