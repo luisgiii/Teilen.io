3aef7f04bfb156a31e93cfc8d7fbde49a693da9b
luisgiii <loiis.x14@gmail.com>
2017-05-30 20:35:47 -0500

    getFeeFromCrypto function added to the API Bridge module.

--- code/core/internalDesign/apiBridge.py
+++ code/core/internalDesign/apiBridge.py
@@ -9,10 +9,17 @@ for an easy access from the main Teilen class.
 import json
 
 def getDataFromTicker(crypto, json_param):
-    cryptoData = dict()
     try:
         with open('tickerJSON', 'r') as ticker:
             cryptoData = json.load(ticker)
             return cryptoData[crypto][json_param]
+    except:
+        return None
+
+def getFeeFromCrypto(crypto, json_param):
+    try:
+        with open('feeJSON', 'r') as fee:
+            feeData = json.load(fee)
+            return feeData[crypto][json_param]
     except:
         return None
