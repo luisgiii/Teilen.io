a57fc57e103cfd7b48351e716a0d2a7adb878efc
luisgiii <loiis.x14@gmail.com>
2017-05-30 20:06:24 -0500

    Test of the Bridge logic.

--- code/core/internalDesign/apiBridge.py
+++ code/core/internalDesign/apiBridge.py
@@ -5,3 +5,11 @@
 Module in charge of the interpretation of the JSON results
 for an easy access from the main Teilen class.
 '''
+
+import json
+
+def getDataFromTicker(crypto, json_param):
+    with open('tickerJSON', 'r') as ticker:
+     cryptoData = json.load(ticker)
+     print cryptoData
+     return crypto[crypto][json_param]
