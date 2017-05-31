25104a8419072c762432d1caff09347828be62ed
luisgiii <loiis.x14@gmail.com>
2017-05-31 00:17:01 +0000

    Name change to tickerDaemon and clean session.

--- code/core/internalDesign/tickerDaemon.py
+++ code/core/internalDesign/tickerDaemon.py
@@ -1,7 +1,6 @@
 import json
 import time
 import requests
-import os
 
 def PoloniexTickerCall(JSONfile):
     response = requests.get("https://poloniex.com/public?command=returnTicker")
@@ -10,11 +9,11 @@ def PoloniexTickerCall(JSONfile):
         json.dump(json_data, JSONfile)
 
 def main():
-    #tickerData = open("tickerJSON", "wb")
-    for repeat in range(0,2):
+    while True:        
         tickerData = open("tickerJSON", "wb")
         PoloniexTickerCall(tickerData)
         time.sleep(3)
         tickerData.close()
+
 if __name__ == '__main__':
     main()
